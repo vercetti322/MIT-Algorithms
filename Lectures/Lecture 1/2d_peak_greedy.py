eada85d1ad221def5c268a374630d3d6b4192965
vercetti322 <vercettitommy322@gmail.com>
2023-08-27 04:59:51 +0530

    configured on Pycharm

--- Lectures/Lecture 1/2d_peak_greedy.py
+++ Lectures/Lecture 1/2d_peak_greedy.py
@@ -1,41 +1,42 @@
-# Calculate 2-D peak of a matrix by using greedy approach assuming it exists
-# Complexity : O(mn)
-
 def find_peak_greedy(matrix):
     '''
-    Input -> a 2D list of lists denoting a matrix
-    Output -> the peak element
-    
-    Algorithm:
-    
-    Start at matrix[0][0] and keep on moving till u find a bigger
-    number, when u can't move any more u have found ur destination.
-    '''
-    i = 0 # row index
-    j = 0 # column index
-    
-    while i < len(matrix) and j < len(matrix[0]):
-        if j < len(matrix[0]) - 1 and (matrix[i][j] < matrix[i][j + 1]):
-            j += 1
-        
-        elif  i < len(matrix) - 1 and matrix[i][j] < matrix[i + 1][j]:
-            i += 1
-            
-        elif i > 0  and matrix[i][j] < matrix[i - 1][j]:
-            i -= 1
+    Find the 2-D peak element in a matrix using the greedy approach, assuming it exists.
 
+    Args:
+        matrix (List[List[int]]): A 2D list denoting a matrix.
+
+    Returns:
+        int: The peak element in the matrix.
+
+    Algorithm:
+        Start at matrix[0][0] and keep moving until a bigger number is found.
+        When movement is not possible, the destination is the peak element.
+    '''
+    i = 0  # row index
+    j = 0  # column index
+
+    while i < len(matrix) and j < len(matrix[0]):
+        if j < len(matrix[0]) - 1 and matrix[i][j] < matrix[i][j + 1]:
+            j += 1
+        elif i < len(matrix) - 1 and matrix[i][j] < matrix[i + 1][j]:
+            i += 1
+        elif i > 0 and matrix[i][j] < matrix[i - 1][j]:
+            i -= 1
         elif j > 0 and matrix[i][j] < matrix[i][j - 1]:
             j -= 1
-            
         else:
             return matrix[i][j]
-        
+
     return -1
 
+
 def main():
+    '''
+    Example usage of the find_peak_greedy function with a sample matrix.
+    '''
     matrix = [[14, 13, 12], [15, 9, 11], [16, 17, 19]]
     print(find_peak_greedy(matrix))
-    
+
 
 if __name__ == '__main__':
     main()
